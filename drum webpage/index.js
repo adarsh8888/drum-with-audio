abcf02df06993f0e0bce00a0e04fbd909b5d33c4
adarsh8888 <110622881+adarsh8888@users.noreply.github.com>
2023-12-26 14:21:34 +0530

    Add files via upload

---
+++ drum webpage/index.js
@@ -0,0 +1,12 @@
+var num=document.querySelectorAll(".drum").length;
+
+for( var i=0; i<num; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var audio=new Audio("sounds/tom-1.mp3") ;
+        audio.play();
+      });
+    
+}
+
+    
+   
